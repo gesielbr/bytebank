08f16609bd69e2b7435b277aa97ed63efe084742
Gesiel Oliveira <gesiel.oliveira@linx.com.br>
2022-03-01 19:00:09 -0300

    Bytebank emitindo valores para outro componente

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,10 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor?: number;
   destino?: number;
+
   transferir() {
-    console.log(`Valor: ${this.valor}`);
-    console.log(`Valor: ${this.destino}`);
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
   }
 }

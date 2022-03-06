9d4ef69a355f68e8a6bd4ff374ed36a02283a69e
Gesiel Oliveira <gesiel.oliveira@linx.com.br>
2022-03-06 16:04:16 -0300

    bytebank integração API fake

--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Transferencia } from './models/transferencia.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TransferenciaService {
   private listaTransferencia: any[];
-  constructor() {
+  private url = 'http://localhost:3000/transferencias';
+
+  constructor(private httpClient: HttpClient) {
     this.listaTransferencia = [];
   }
 
   get transferencias() {
     return this.listaTransferencia;
+  }
+
+  todas(): Observable<Transferencia[]> {
+    return this.httpClient.get<Transferencia[]>(this.url);
   }
 
   adicionarTransferencia(transferencia: any) {
